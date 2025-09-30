97a7bac0c8c1f677ec19fccf26d229eea14e6c22
anmol <anmolsahota05760@gmail.com>
2025-09-30 16:54:19 +0530

    changes in code for testing CI CD

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        test this for CI CD
         <img src={logo} className="App-logo" alt="logo" />
         <p>{message || "Click the button to get a message from backend"}</p>
         <button onClick={fetchMessage}>Get Message</button>
