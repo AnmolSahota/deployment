5c687439fa20237b255f5d3461ca48340b7f902d
anmol <anmolsahota05760@gmail.com>
2025-09-30 20:04:28 +0530

    changes in server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Home Route" });
+});
+
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello to you from Express Server on EC2 Server!" });
 });
